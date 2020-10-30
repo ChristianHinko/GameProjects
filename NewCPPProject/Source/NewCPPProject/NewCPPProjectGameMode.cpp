978d03f722735c9c55cc0611a8763c24c9aa99a3
include-iostream <c4hinkle@gmail.com>
2020-10-30 04:02:34 -0300

    add first part of NewCPPProject

---
+++ NewCPPProject/Source/NewCPPProject/NewCPPProjectGameMode.cpp
@@ -0,0 +1,13 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "NewCPPProjectGameMode.h"
+
+#include "NewCPPProjectCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+
+
+ANewCPPProjectGameMode::ANewCPPProjectGameMode()
+{
+
+}
